a0ba18d319f7f35894de704f0e149d9747cb094d
Musawir Shah <musawir@gmail.com>
2013-11-17 17:00:13 -0800

    Initial Commit

---
+++ MapTour/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapTour
+//
+//  Created by Musawir Shah on 11/17/13.
+//  Copyright (c) 2013 Musawir Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
